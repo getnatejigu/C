120a62c01d81236794adb59e1136e1ef434a0aa5
Getnat Ejigu <getnatejigu@gmail.com>
2018-11-15 08:49:43 -0500

    merge sort implementation

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -24,8 +24,7 @@ void merge(int *arr, int *aux, int lo, int mid, int hi)
 	i = lo;
 	j = mid + 1;
 
-	//printf("merge: lo: %d mid: %d  hi: %d \n", lo, mid, hi);
-
+	/* merge sorted halves */
 	for(k = lo; k <= hi; k++) {
 		if (i > mid) {
 			arr[k] = aux[j++];
@@ -52,8 +51,6 @@ void merge_sort(int *arr, int *aux, int lo, int hi)
 
 	int mid = lo + (hi - lo) / 2;
 
-	//printf(" lo: %d  mid: %d  hi: %d \n", lo, mid, hi);
-
 	merge_sort(arr, aux, lo, mid);
 	merge_sort(arr, aux, mid + 1, hi);
 	merge(arr, aux, lo, mid, hi);
@@ -61,17 +58,21 @@ void merge_sort(int *arr, int *aux, int lo, int hi)
 
 void sort(int *arr, int size)
 {
-	int aux[ARY_SIZE];
+	int aux[ARY_SIZE]; /* temporary array */
+
+	/* begin sorting here */
 	merge_sort(arr, aux, 0, ARY_SIZE - 1);
 }
 
 int main() 
 {
+	/* array to be sorted */
 	int arr[ARY_SIZE] = {4, 4, 5, 2, 9, 3, 2, 2, 8, 0};
 
 	printf("Unsorted: \n");
 	print_array(arr, ARY_SIZE);
 
+	/* generic sort method */
 	sort(arr, ARY_SIZE);
 
 	printf("Sorted: \n");
